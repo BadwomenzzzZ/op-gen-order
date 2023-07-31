46c015e92d9cf988f4383e841f92ee5d9a2305f7
chenjin <2983402293@qq.com>
2023-07-31 19:01:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# op-gen-order
---
+++ src/main/java/com/genorder/OpGenOrderApplication.java
@@ -0,0 +1,13 @@
+package com.genorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpGenOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpGenOrderApplication.class, args);
+    }
+
+}
